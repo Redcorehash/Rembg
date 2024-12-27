8bf3ddc9eeb16bfdf91e292ff934317a8ae58268
Jjfjdjdjc <ry76rt3ye@yandex.com>
2024-12-27 17:42:31 +0700

    Create imageUtils.js

---
+++ src/utils/imageUtils.js
@@ -0,0 +1,18 @@
+export const compressImage = (file, quality = 0.8) => {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const img = new Image();
+            img.src = event.target.result;
+            img.onload = () => {
+                const canvas = document.createElement('canvas');
+                canvas.width = img.width;
+                canvas.height = img.height;
+                const ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0);
+                canvas.toBlob((blob) => resolve(blob), 'image/jpeg', quality);
+            };
+        };
+        reader.readAsDataURL(file);
+    });
+};
