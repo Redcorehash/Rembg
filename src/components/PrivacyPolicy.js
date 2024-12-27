3b9f57a5c07b33e29f285e20048c850b377c247a
Jjfjdjdjc <ry76rt3ye@yandex.com>
2024-12-27 15:43:39 +0700

    Create PrivacyPolicy.js

---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../styles/globals.css';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className={styles.privacyPolicy}>
+            <h1>Privacy Policy</h1>
+            <p>
+                Your privacy is important to us. This privacy policy explains how we collect,
+                use, and protect your personal information.
+            </p>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
